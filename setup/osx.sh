cfb85ebee00371c8e634b8c1846dd3c5858282b5
phette23 <phette23@gmail.com>
2014-07-22 14:05:07 -0700

    osx: activate cron

--- setup/osx.sh
+++ setup/osx.sh
@@ -32,6 +32,9 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# Activate cron
+sudor mkdir /etc/cron
+
 ###############################################################################
 # General UI/UX                                                               #
 ###############################################################################
