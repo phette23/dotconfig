17d7526b00bd13319f6e827c9ef58da3f9780006
phette23 <phette23@gmail.com>
2014-08-17 20:21:25 -0700

    apt-get: split into 2 sets of apt-get installs, desktop & server

--- setup/desktop-apt.sh
+++ setup/desktop-apt.sh
@@ -1,59 +1,34 @@
 #!/usr/bin/env bash
-# Script to grab major software for a fresh Ubuntu install
+# Software for a fresh Ubuntu Desktop install
 
-# First update apt-get
 sudo apt-get update
 
-# Install all my packages
-# -y = "assume yes to all prompts & run non-interactively"
 sudo apt-get -y install \
-	adobe-flashplugin \
 	apache2 \
+	adobe-flashplugin \
 	audacity \
 	chromium-browser \
 	clang \
-	cowsay \
-	curl \
-  	denyhosts \
 	epiphany-browser \
 	fonts-droid \
 	fonts-inconsolata \
 	fonts-linuxlibertine \
 	freeciv-server \
 	freeciv-client-gtk \
-	git \
 	gimp \
 	gnome-do \
 	gnome-dictionary \
 	inkscape \
-	lynx \
 	midori \
-	mongodb \
-	mysql-server \
-	ngrep \
-  	openssh-server \
-	optipng \
-	pandoc \
 	phantomjs \
-	php5-cli \
-	php-pear \
-	pngcrush \
-	python-setuptools \
-	rbenv \
-	ruby1.9.1 \
 	scribus \
-	source-highlight \
 	synaptic \
 	terminator \
 	texlive-fonts-recommended \
 	tor \
-	tree \
 	vidalia \
-	vim \
 	wine \
-	xclip \
-  	xinetd \
-	yaz
+	xclip
 
 # Done! Dropbox is the only major item not in apt-get
 echo "Done! Don't forget to manually install Dropbox."
---
+++ setup/server-apt.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Software for a fresh Ubuntu Server install
+
+sudo apt-get update
+
+sudo apt-get -y install \
+    cowsay \
+    curl \
+    denyhosts \
+    git \
+    lynx \
+    mongodb \
+    ngrep \
+    openssh-server \
+    optipng \
+    pandoc \
+    php5-cli \
+    php-pear \
+    pngcrush \
+    python-setuptools \
+    rbenv \
+    ruby1.9.1 \
+    source-highlight \
+    tree \
+    vim \
+    xinetd \
+    yaz
