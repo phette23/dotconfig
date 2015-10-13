e00117299acbe13b592e12c05a094331c59fc106
phette23 <phette23@gmail.com>
2015-10-13 09:41:28 -0700

    npm - rm cssmin, express-gen, prefix setting

--- setup/npm.sh
+++ setup/npm.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
-
-npm config set prefix ~/npm
-
 # install global NPM packages
 npm install -g --quiet \
   bower \
-  cssmin \
-  express-generator \
   generator-h5bp \
   generator-node \
   generator-reveal \
