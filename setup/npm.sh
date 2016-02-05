911de25224958886c6ed7b411afbd0543b8d02ab
phette23 <phette23@gmail.com>
2016-02-04 18:25:51 -0800

    update npm packages

--- setup/npm.sh
+++ setup/npm.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 # install global NPM packages
 npm install -g --quiet \
-  babel \
-  bower \
+  babel-cli \
   generator-h5bp \
   generator-node \
   generator-reveal \
   grunt-cli \
-  gulp \
   jshint \
   nodemon \
-  phantomas \
   trash-cli \
   uglify-js \
   yo
