8beb05e8146ddc902b40fdb7b984eca473eef79b
phette23 <phette23@gmail.com>
2017-03-26 13:31:34 -0700

    brew.sh - rm rbenv, nvm cmds as they don't work
    rbenv & nvm need to be sourced during shell startup
    they're not installed binaries, so these don't work

--- setup/brew.sh
+++ setup/brew.sh
@@ -48,9 +48,3 @@ brew cleanup
 echo '/usr/local/bin/bash' | sudo tee -a /etc/shells
 echo '/usr/local/bin/fish' | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/bash
-
-# Get a current ruby
-rbenv install 2.4.1
-# and a current node
-nvm install stable
-# add a current perl here, too?
