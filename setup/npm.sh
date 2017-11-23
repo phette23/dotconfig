1319c65b465a0b70001c5dbfe5273af1bfdfed3b
phette23 <phette23@gmail.com>
2017-11-23 13:02:16 -0800

    npm - couple package updates

--- setup/npm.sh
+++ setup/npm.sh
@@ -3,12 +3,13 @@
 npm install -g --quiet \
   babel-cli \
   grunt-cli \
+  gulp-cli \
   n \
   trash-cli \
   uglify-js
 
 # if any of my personal utilities exist, link them
-projects="~/code/dogedc ~/code/equella-cli"
+projects="~/code/dogedc ~/code/equella_cli"
 for project in ${projects}; do
     # if dir exists, move into it & link
     [ -d ${project} ] && cd ${project} && npm link
