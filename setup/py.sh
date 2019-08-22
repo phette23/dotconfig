ef1668658a0e53fe14834eab522da1115f2d51ca
phette23 <phette23@gmail.com>
2019-08-21 20:57:38 -0700

    pip updates
    install: virtualfish
    remove: flake8, pymarc, requests
    those should all be installed in project dirs not globally
    also I don't think easy_install pip is necessary anymore

--- setup/py.sh
+++ setup/py.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash -x
 # setup Python modules
 
-sudo easy_install pip
-
 sudo -H pip install \
   csvkit \
-  flake8 \
-  pymarc \
-  requests \
-  virtualenv
+  virtualenv \
+  virtualfish
