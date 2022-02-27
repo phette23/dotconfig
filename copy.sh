8f73224260fb7fa79914c4b7dc883158f7894cc2
phette23 <phette23@gmail.com>
2022-02-27 15:13:53 -0800

    reorganize & add .ssh/config

--- copy.sh
+++ copy.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-mkdir -p ~/bin
-cp -vr bin/* ~/bin
-cp -vr config/. ~
-mkdir ~/.config && mv ~/emmet ~/.config
-cp -vr .vim/. ~/.vim
-cp -vr .pip/. ~/.pip
+rsync -avz --progress config/. ~
