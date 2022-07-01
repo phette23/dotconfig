2129003e46c0f72cde75d55f1e2ffe8fe61bae99
phette23 <phette23@gmail.com>
2022-07-01 16:01:36 -0700

    npm - more conversion to pnpm work

--- setup/npm.sh
+++ setup/npm.sh
@@ -3,7 +3,8 @@
 sudo mkdir /usr/local/n && sudo chown $(whoami):staff /usr/local/n
 
 # get pnpm and switch to using it for global deps
-corepack enable
+corepack enable pnpm
+pnpm setup
 # install node stable with n
 pnpm install -g n
 n stable
@@ -11,10 +12,3 @@ pnpm install -g fx trash-cli
 
 # remove homebrew node, we will use n for node version management
 brew uninstall node
-
-# if any of my personal utilities exist, link them
-projects="~/code/dogedc ~/code/equella_cli"
-for project in ${projects}; do
-    # if dir exists, move into it & link
-    [ -d ${project} ] && cd ${project} && pnpm link
-done
