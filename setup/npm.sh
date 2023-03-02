140a65c9d75bce5a71156ad749c0690e48488a45
phette23 <phette23@gmail.com>
2023-03-01 21:32:17 -0800

    npm - fix the pnpm & n install process

--- setup/npm.sh
+++ setup/npm.sh
@@ -3,12 +3,14 @@
 sudo mkdir /usr/local/n && sudo chown $(whoami):staff /usr/local/n
 
 # get pnpm and switch to using it for global deps
-corepack enable pnpm
+npm i -g pnpm
 pnpm setup
+export PNPM_HOME="~/Library/pnpm"
+export PATH="$PNPM_HOME":$PATH
 # install node stable with n
 pnpm install -g n
 n stable
 pnpm install -g fx trash-cli
 
 # remove homebrew node, we will use n for node version management
-brew uninstall node
+echo "If that this suceeded, you can remove the hombrew node with 'brew uninstall node'"
