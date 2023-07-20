806d3197bb66cba06772e069436529eb10a385f5
phette23 <phette23@gmail.com>
2023-07-20 15:22:19 -0700

    py: add unoconv, use pyenv for global python
    note about why we're not using homebrew python globally

--- setup/py.sh
+++ setup/py.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash -x
 # setup Python modules
-python3 -m pip install --upgrade pip
-python3 -m pip install csvkit
+# we use pyenv to get a modern, non-homebrew python, then use that as our global python version
+# it's a bad idea to use homebrew python for global packages because reguler `brew upgrade` cmds
+# will wipe out your existing python & break packages installed under it
+# https://justinmayer.com/posts/homebrew-python-is-not-for-you/
+pyenv install 3.11 && pyenv global 3.11
+python -m pip install --upgrade pip
+python -m pip install csvkit \
+    unoconv
