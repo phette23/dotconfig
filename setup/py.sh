6ae479178bb6f295345219daf2be505334891429
phette23 <phette23@gmail.com>
2023-10-20 10:22:34 -0700

    py: install python 2.7

--- setup/py.sh
+++ setup/py.sh
@@ -4,7 +4,10 @@
 # it's a bad idea to use homebrew python for global packages because reguler `brew upgrade` cmds
 # will wipe out your existing python & break packages installed under it
 # https://justinmayer.com/posts/homebrew-python-is-not-for-you/
-pyenv install 3.11 && pyenv global 3.11
+pyenv install 3.11
+# need python 2 for old EQUELLA scripts
+pyenv install 2.7
+pyenv global 3.11 2.7
 pyenv init - | source
 python -m pip install --upgrade pip
 python -m pip install csvkit \
