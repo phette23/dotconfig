7df0d1e349dd68d2f26b2a3d3fffa3c0a7a3b5c9
phette23 <phette23@gmail.com>
2024-07-01 16:54:02 -0700

    refactor: migrate lang scripts to fish
    also check for existence of mise before running mise cmds

--- setup/py.fish
+++ setup/py.fish
@@ -1,14 +1,17 @@
 #!/usr/bin/env fish
-# Python setup
 # Use fish shell so we can modify its PATH with `fish_add_path` & not need to edit config.fish
 # like the `pipx ensurepath` command does. We get mise and pipx from homebrew, install python
 # versions with mise, then use pipx to install global python packages.
 # https://justinmayer.com/posts/homebrew-python-is-not-for-you/
-# TODO check for mise
-mise install python@3.12
-mise install python@2.7
-mise global python@3.12 python@2.7
-mise reshim python
+if command --query mise
+    mise install python@3.12
+    mise install python@2.7
+    mise global python@3.12 python@2.7
+    mise reshim python
+else
+    echo "mise not found, run brew.sh first to install mise, then run this script again."
+    exit 1
+end
 
 if command --query pipx
     fish_add_path ~/.local/bin
