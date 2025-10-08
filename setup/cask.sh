8dd4329dda97c92209870c2edc54742a360eb141
phette23 <phette23@gmail.com>
2025-10-08 08:50:25 -0700

    fix: rm unused casks
    Spotlight does everything I used Alfred for
    libreoffice might return but for now I don't need it
    the quicklook plugins are all deprecated and not necessary

--- setup/cask.sh
+++ setup/cask.sh
@@ -12,23 +12,14 @@ bic () { brew install --cask "$1"; }
 
 [ -d /Applications/1Password.app ] || bic 1password
 bic 1password/tap/1password-cli
-bic alfred
 bic docker-desktop
 bic discord
 bic firefox
 [ -d "/Applications/Google Chrome.app" ] || bic google-chrome
 bic imageoptim
 bic iterm2
-bic libreoffice # unoconv dependency
 bic slack
 bic spotify
 bic tor-browser
 bic visual-studio-code
 bic xquartz
-
-# useful Quick Look plugins for developers
-# https://github.com/sindresorhus/quick-look-plugins
-bic qlmarkdown
-bic quicklook-json
-bic quicklook-csv
-bic webpquicklook
