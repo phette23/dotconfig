4729f4854119355cf4f83efb070164c52c2e2501
phette23 <phette23@gmail.com>
2026-05-07 10:07:21 -0700

    fix: pnpm v11 setup

--- setup/npm.fish
+++ setup/npm.fish
@@ -8,13 +8,16 @@ else
 end
 
 # get pnpm and switch to using it for global deps
-npm i -g pnpm
+if command --query corepack
+    corepack enable pnpm
+else
+    npm i -g pnpm@latest
+end
 set -Ux PNPM_HOME "$HOME/Library/pnpm"
 fish_add_path "$PNPM_HOME"
 pnpm setup
-pnpm install -g \
-    fx \
-    trash-cli
+pnpm add -g \
+    fx
 npm config set --global fund false
 pnpm config set --global fund false
 pnpm config set --global minimumReleaseAge (math 60 x 24 x 7)
